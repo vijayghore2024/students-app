66d1f7093b45416061ea5d88c640c370940ef154
vijayghore2024 <vijayghore2024@gmail.com>
2024-01-02 09:49:31 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -5,11 +5,37 @@ import AddStudent from "./components/AddStudent";
 import ViewStudent from "./components/ViewStudent";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import { useEffect, useState } from "react";
+import userEvent from "@testing-library/user-event";
 
 function App() {
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    showAlert();
+  }, [count])
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  
+  const showAlert = () => {
+    alert("React App Loaded..")
+  }
+  
   return (
+
+
     <div className="">
-      <BrowserRouter>
+
+          <div className="container">
+              <p className="display-1">{count}</p>
+
+              <button className="btn btn-dark" onClick={() => handleClick()}>Count++</button>
+          </div>
+
+
+
+      {/* <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,7 +43,7 @@ function App() {
           <Route path="/addstudent" element={<AddStudent />} />
           <Route path="/viewstud" element={<ViewStudent />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
